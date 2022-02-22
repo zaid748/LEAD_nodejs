d21e5c66f845d424e743f84bab9975d182611ff2
zaid748 <zaidnevarez@gmail.com>
2022-02-22 08:14:13 -0700

    buscando errosres en department

--- src/controllers/departamentos.controller.js
+++ src/controllers/departamentos.controller.js
@@ -4,8 +4,6 @@ const Departamentos = require('../models/Departamentos');
 
 departamentoCtrl.mostrarDepartamentos = async(req, res, next) =>{
     const departments = await Departamentos.find();
-    const hola = 'hola depastamentos'
-    console.log(departments, hola);
     return next();
 }
 
