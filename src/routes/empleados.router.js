84f479252c994f4aed59d31d74432bd4a3bae2dd
zaid748 <zaidnevarez@gmail.com>
2022-02-23 14:01:23 -0700

    trabajando en roles

--- src/routes/empleados.router.js
+++ src/routes/empleados.router.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const { agregarEmpleadoView, agregarEmpleado, employedView, ViewInfo } = require('../controllers/empleados.contreller');
-const {isAuthenticated} = require('../helpers/auth');
+const {isAuthenticated, isAustheAdministrator} = require('../helpers/auth');
 
-router.get('/empleados/Agregar' , isAuthenticated , agregarEmpleadoView);
+router.get('/empleados/Agregar' , isAuthenticated, isAustheAdministrator , agregarEmpleadoView);
 
-router.post('/empleados/Agregar', isAuthenticated, agregarEmpleado);
+router.post('/empleados/Agregar', isAuthenticated, isAustheAdministrator, agregarEmpleado);
 
-router.get('/empleados', isAuthenticated, employedView)
+router.get('/empleados', isAuthenticated, isAustheAdministrator,employedView)
 
-router.get('/empleado/:id', isAuthenticated, ViewInfo);
+router.get('/empleado/:id', isAuthenticated, isAustheAdministrator, ViewInfo);
 
 module.exports = router;
