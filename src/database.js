ea0a8345d7607ee3ba6a43242a6f338fbf209b47
zaid748 <zaidnevarez7@gmail.com>
2023-01-11 18:38:17 -0700

    trabajando con actualizacion de npm

--- src/database.js
+++ src/database.js
@@ -4,9 +4,11 @@ const {NOTES_APP_MONGODB_HOST} = process.env;
 
 const MONGODB_URI = `${NOTES_APP_MONGODB_HOST}`;
 
+mongoose.set('strictQuery', true);
+
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 })
-    .then(db => console.log('DB is connected'))
+    .then(db => console.log('DB is connected',  db))
     .catch(err => console.log(err));
