54b38d8ec4e189938f4439a5619ce7d7bdf6d673
zaid748 <zaidnevarez7@gmail.com>
2025-03-27 18:21:41 -0600

    se agregan documentos de docker

--- src/server.js
+++ src/server.js
@@ -49,7 +49,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173' || 'https://lead-inmobiliaria.com:4000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
